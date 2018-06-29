06148e719e28505b3462ddc2ab234ca86c9120e9
rikiparata <rikiipee47@gmail.com>
2018-06-29 15:48:15 +1200

    Website

---
+++ Style.cs
@@ -0,0 +1,32 @@
+body {
+  background-image:url("https://www.advantour.com/russia/images/symbolics/russia-flag.jpg");
+  height=100%;
+}
+.banner{
+  text-align: center;
+  margin: auto;
+  
+}
+p {
+  font-family: arial;
+  color: white;
+  text-align:center;
+}
+.matchkey{
+  font-family: "Times New Roman";
+  color: white;
+  text-align: center;
+}
+h2{
+  text-align: center;
+}
+.game{
+  text-align: center:
+  color: whitel;
+}
+.video{
+  margin: auto;
+}
+.image{
+  margin: auto;
+}
